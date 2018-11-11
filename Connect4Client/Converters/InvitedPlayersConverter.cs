f5b738bdbd6a6c4a80a393e7535ea5b2e2d34908
pappbence96 <pappbence96@sch.bme.hu>
2018-11-11 13:41:54 +0100

    -Added temporary DTO classes -Created Lobby selection interface -Experimenting with Match interface

---
+++ Connect4Client/Converters/InvitedPlayersConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Connect4Client.Converters {
+    class InvitedPlayersConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            var invitedPlayers = value as List<String>;
+            if(value == null) {
+                return "No players invited";
+            }
+            return string.Join("\n", invitedPlayers);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Connect4Client/DTOs/LobbyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4Client.DTOs {
+    class LobbyData {
+        private List<String> invitedPlayers = new List<string>();
+
+        public int Id { get; set; }
+        public String Leader { get; set; }
+        public bool Open { get; set; }
+        public List<String> InvitedPlayers { get { return invitedPlayers; } set { invitedPlayers = value; } }
+    }
+}
