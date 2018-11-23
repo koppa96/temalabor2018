f382f25603f14d92c9aa9ca0be50309a68cc94b1
Papp Bence <pappbence96@gmail.com>
2018-11-23 22:37:32 +0100

    -Tried to fix list, broke it in another way

--- Connect4Client/App.xaml.cs
+++ Connect4Client/App.xaml.cs
@@ -67,10 +67,12 @@ namespace Connect4Client
             {
                 if (rootFrame.Content == null)
                 {
+
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(LoginPage), e.Arguments);
+                    //rootFrame.Navigate(typeof(LoginPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
