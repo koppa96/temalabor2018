902318424d6d99b84a83287bdd907bd72017f969
Péter Koppa <koppa96@windowslive.com>
2019-02-24 12:26:29 +0100

    Upgrading server to .NET Core 2.2

--- Connect4Server/Startup.cs
+++ Connect4Server/Startup.cs
@@ -66,7 +66,7 @@ namespace Connect4Server {
                 };
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSignalR();
 
