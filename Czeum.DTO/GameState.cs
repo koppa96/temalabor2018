29ff0932e9a3fc22a4bcd37225bb6c2a1b438cd5
Péter Koppa <koppa96@windowslive.com>
2019-03-19 16:06:06 +0100

    Wrapping MoveResult into a MatchStatus object that provides context about the move

---
+++ Czeum.DTO/GameState.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Czeum.DTO {
+	public enum GameState {
+		YourTurn, EnemyTurn, YouWon, EnemyWon, Draw
+	}
+}
---
+++ Czeum.DTO/MatchStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Czeum.Abstractions;
+
+namespace Czeum.DTO
+{
+    public class MatchStatus
+    {
+        public int MatchId { get; set; }
+        public string OtherPlayer { get; set; }
+        public MoveResult CurrentBoard { get; set; }
+        public GameState State { get; set; }
+    }
+}
