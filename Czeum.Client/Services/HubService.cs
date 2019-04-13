5cc30b9bbb02cafaa30c353907d220aa2ebc6d5c
pappbence96 <pappbence96@sch.bme.hu>
2019-04-13 15:39:57 +0200

    Added HubService stub.

---
+++ Czeum.Client/Services/HubService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Czeum.Client.Interfaces;
+using Prism.Windows.Navigation;
+
+namespace Czeum.Client.Services {
+    class HubService : IHubService {
+        private INavigationService navigationService;
+        private IDialogService dialogService;
+        private ILobbyService lobbyService;
+
+        public HubService(INavigationService navigationService, IDialogService dialogService,
+            ILobbyService lobbyService)
+        {
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+            this.lobbyService = lobbyService;
+        }
+
+
+    }
+}
