2c3a8b70c2416a33e01f6723bc1795eb6124d712
pappbence96 <pappbence96@sch.bme.hu>
2019-04-13 11:35:28 +0200

    IDialogService supports Confirmation, Error, Success and Progress dialogs.

--- Czeum.Client/Interfaces/IDialogService.cs
+++ Czeum.Client/Interfaces/IDialogService.cs
@@ -9,5 +9,9 @@ using Windows.UI.Xaml.Controls;
 namespace Czeum.Client.Interfaces {
     public interface IDialogService {
         IAsyncOperation<ContentDialogResult> ShowConfirmation(string message);
+        IAsyncOperation<ContentDialogResult> ShowSuccess(string message);
+        IAsyncOperation<ContentDialogResult> ShowError(string message);
+        void ShowLoadingDialog();
+        void HideLoadingDialog();
     }
 }
