d987fb874db82832957cce7ec2e4e684c8f736d8
Péter Koppa <koppa96@windowslive.com>
2019-05-15 15:18:16 +0200

    Added missing exception declarations in comments

--- Czeum.Server/Services/ServiceContainer/IServiceContainer.cs
+++ Czeum.Server/Services/ServiceContainer/IServiceContainer.cs
@@ -1,6 +1,7 @@
 using Czeum.Abstractions.DTO;
 using Czeum.Abstractions.GameServices;
 using Czeum.DAL.Entities;
+using Czeum.Abstractions;
 
 namespace Czeum.Server.Services.ServiceContainer
 {
@@ -10,28 +11,31 @@ namespace Czeum.Server.Services.ServiceContainer
     public interface IServiceContainer
     {
         /// <summary>
-        /// Finds the game service that can execute the passed MoveData
+        /// Finds the game service that can execute the passed MoveData.
         /// </summary>
         /// <param name="moveData">The move to be executed</param>
+        /// <exception cref="GameNotSupportedException">Thrown when there is no suitable service for the move</exception>
         /// <returns>The correct game service</returns>
         IGameService FindByMoveData(MoveData moveData);
         
         /// <summary>
-        /// Finds the game service that can generate a default board for the passed LobbyData
+        /// Finds the game service that can generate a default board for the passed LobbyData.
         /// </summary>
         /// <param name="lobbyData">The lobby data</param>
+        /// <exception cref="GameNotSupportedException">Thrown when there is no suitable service for the lobby</exception>
         /// <returns>The correct game service</returns>
         IGameService FindByLobbyData(LobbyData lobbyData);
 
         /// <summary>
-        /// Finds the game service that can transform the serialized board into a MoveResult
+        /// Finds the game service that can transform the serialized board into a MoveResult.
         /// </summary>
         /// <param name="serializedBoard">The serialized board</param>
+        /// <exception cref="GameNotSupportedException">Thrown when there is no suitable service for the board</exception>
         /// <returns>The correct game service</returns>
         IGameService FindBySerializedBoard(SerializedBoard serializedBoard);
         
         /// <summary>
-        /// Gets a random game service from the contained services
+        /// Gets a random game service from the contained services.
         /// </summary>
         /// <returns>The random game service</returns>
         IGameService GetRandomService();
