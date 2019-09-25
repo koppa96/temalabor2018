3ef34cfad9e2a146d4b73fb699e8e659eca0a98d
Péter Koppa <koppa96@windowslive.com>
2019-09-25 21:36:59 +0200

    Updating to .NET Core 3.0

--- Czeum.DTO/Wrappers/Wrapper.cs
+++ Czeum.DTO/Wrappers/Wrapper.cs
@@ -2,6 +2,10 @@ using Czeum.DTO.Lobbies;
 
 namespace Czeum.DTO.Wrappers
 {
+    /// <summary>
+    /// Wraps an object together with a GameType discriminator
+    /// </summary>
+    /// <typeparam name="TContent">The type of the content</typeparam>
     public class Wrapper<TContent>
     {
         public GameType GameType { get; set; }
