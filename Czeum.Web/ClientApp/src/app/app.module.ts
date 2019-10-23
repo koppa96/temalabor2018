24d0d79369ebf1f343a340f839edb99770218b4e
Péter Koppa <koppa96@windowslive.com>
2019-10-23 20:40:38 +0200

    Creating auth guard and interceptor

--- Czeum.Web/ClientApp/src/app/app.module.ts
+++ Czeum.Web/ClientApp/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './components/home/home.component';
+import {AuthService} from './services/auth.service';
+import {AuthInterceptor} from './interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,11 @@ import { HomeComponent } from './components/home/home.component';
       { path: 'home', component: HomeComponent }
     ])
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
