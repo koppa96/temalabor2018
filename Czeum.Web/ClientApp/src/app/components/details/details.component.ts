4e2ac153de24a69e4ed4a2fbbf34e7c0544ac085
koppa96 <koppa96@windowslive.com>
2019-10-26 11:18:20 +0200

    Created links to details, and scrolling

--- Czeum.Web/ClientApp/src/app/components/details/details.component.ts
+++ Czeum.Web/ClientApp/src/app/components/details/details.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, AfterViewInit {
+  @ViewChild('chess', { static: false }) chessTitle: ElementRef;
+  @ViewChild('connect4', { static: false }) connect4Title: ElementRef;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.route.queryParams.subscribe(
+      query => {
+        if (query.scrollTo === 'Chess') {
+          this.chessTitle.nativeElement.scrollIntoView();
+        } else if (query.scrollTo === 'Connect4') {
+          this.connect4Title.nativeElement.scrollIntoView();
+        }
+      }
+    );
   }
 
 }
