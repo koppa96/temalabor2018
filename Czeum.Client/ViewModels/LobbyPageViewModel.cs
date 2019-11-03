a28c40dedf1b123ecf3e0e4b9bfd1fd3d3037578
Bence Papp <pappbence96@gmail.com>
2019-11-03 23:14:56 +0100

    Added (hacked?) filter on lobby list.

--- Czeum.Client/ViewModels/LobbyPageViewModel.cs
+++ Czeum.Client/ViewModels/LobbyPageViewModel.cs
@@ -3,27 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using Windows.UI.Core;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 using Prism.Commands;
 using Prism.Logging;
 using Prism.Windows.Navigation;
 using Czeum.Core.ClientCallbacks;
-using Windows.ApplicationModel.Core;
-using Czeum.Core.DTOs;
 using Czeum.Core.DTOs.Abstractions.Lobbies;
-using Czeum.Core.DTOs.Chess;
-using Czeum.Core.DTOs.Connect4;
 using Czeum.Core.Services;
 using Czeum.Client.Interfaces;
 using Czeum.Core.Enums;
 using Czeum.Core.DTOs.Wrappers;
 using Flurl.Http;
+using Czeum.Core.DTOs.Extensions;
 
 namespace Czeum.Client.ViewModels {
     public class LobbyPageViewModel : ViewModelBase
@@ -40,7 +31,21 @@ namespace Czeum.Client.ViewModels {
         public ILobbyStore lobbyStore { get; private set; }
 
         public ObservableCollection<LobbyData> LobbyList { get => lobbyStore.LobbyList; }
+
+
+        private ObservableCollection<LobbyData> filteredList = new ObservableCollection<LobbyData>();
+        public ObservableCollection<LobbyData> FilteredList {
+            get => filteredList; 
+            private set {
+                filteredList = value;
+                RaisePropertyChanged();
+            }
+        }
         public string Username { get => userManagerService.Username; }
+        public string NameFilter { get; set; } = "";
+        public bool HidePrivate { get; set; } = false;
+        public bool FilterChess { get; set; } = false;
+        public bool FilterConnect4 { get; set; } = false;
 
         public LobbyPageViewModel(ILobbyService lobbyService,
                                   INavigationService navigationService,
@@ -67,13 +72,39 @@ namespace Czeum.Client.ViewModels {
 
             JoinLobbyCommand = new DelegateCommand<Guid?>(JoinLobby);
             CreateLobbyCommand = new DelegateCommand<string>(CreateLobby);
+            ApplyFiltersCommand = new DelegateCommand(FilterLobbyList);
+            ClearFiltersCommand = new DelegateCommand(ClearFilters);
+
+            FilteredList = new ObservableCollection<LobbyData>(LobbyList.AsEnumerable());
+            lobbyStore.LobbyList.CollectionChanged += (s, e) => FilterLobbyList();
         }
+
 
         public ICommand JoinLobbyCommand { get; }
         public ICommand CreateLobbyCommand { get; }
+        public ICommand ApplyFiltersCommand { get; }
+        public ICommand ClearFiltersCommand { get; }
 
 
+        private void ClearFilters()
+        {
+            HidePrivate = false;
+            NameFilter = "";
+            FilterLobbyList();
+        }
 
+        private void FilterLobbyList()
+        {
+            FilteredList = new ObservableCollection<LobbyData>(LobbyList.AsEnumerable().Where(l => 
+                (!HidePrivate || (l.Access != LobbyAccess.Private)) // Only perform filtering by visibility if HidePrivate is set
+                &&
+                (l.Name.Contains(NameFilter))
+                &&
+                (!(FilterChess || FilterConnect4) || // Only perform filtering if at least one type-filter is ticked, else ignore
+                    (FilterChess && l.GetGameType() == GameType.Chess) || (FilterConnect4 &&l.GetGameType() == GameType.Connect4) // I am not proud of this
+                )
+            ));
+        }
         private async void JoinLobby(Guid? id)
         {
             if (!id.HasValue)
