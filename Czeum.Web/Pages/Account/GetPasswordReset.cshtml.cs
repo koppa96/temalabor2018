6162e6af9508d9f042a26f084cc13152c0b74bc6
Péter Koppa <koppa96@windowslive.com>
2020-03-27 18:05:34 +0100

    Create password reset pages

---
+++ Czeum.Web/Pages/Account/GetPasswordReset.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Czeum.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Czeum.Web.Pages.Account
+{
+    public class GetPasswordResetModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+
+        [Required(ErrorMessage = "Kötelező")]
+        [EmailAddress(ErrorMessage = "Nem érvényes e-mail cím")]
+        [BindProperty]
+        public string Email { get; set; } = "";
+
+        [BindProperty]
+        public string? ReturnUrl { get; set; } = "";
+
+        public bool SendSuccessful { get; set; }
+
+        public GetPasswordResetModel(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public void OnGet(string? returnUrl)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string action)
+        {
+            if (action == "cancel")
+            {
+                return Redirect(ReturnUrl);
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError(nameof(Email), "Nem létezik ilyen e-mail című felhasználó");
+                }
+                else
+                {
+                    var token = await userManager.GeneratePasswordResetTokenAsync(user);
+
+                    // TODO: Send e-mail
+
+                    SendSuccessful = true;
+                }
+            }
+
+            return Page();
+        }
+    }
+}
